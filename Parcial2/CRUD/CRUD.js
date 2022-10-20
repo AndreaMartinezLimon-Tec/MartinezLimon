a9ac3d8bd374ea546c4fb602ee85b209592e5ac4
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-10-20 18:08:42 -0500

    CRUD

--- Parcial2/CRUD/CRUD.js
+++ Parcial2/CRUD/CRUD.js
@@ -14,24 +14,30 @@ const app = express();
 app.use(express.text())
 app.use(express.json())
 
-app.get('/consulta/:id_Ingrediente', async (req,res) => {
+app.get('/ingrediente', async (req,res) => {
+    const ID = req.params.id_Ingrediente;
+    const [responseBD] = await pool.execute(`SELECT * FROM ingrediente`);
+    res.json(responseBD);
+});
+
+app.get('/ingrediente/:id_Ingrediente', async (req,res) => {
     const ID = req.params.id_Ingrediente;
     const [responseBD] = await pool.execute(`SELECT * FROM ingrediente WHERE id_Ingrediente=${ID}`);
     res.json(responseBD);
 });
-app.post('/registro', async (req,res) => {
+app.post('/ingrediente', async (req,res) => {
     const [responseBD] = await pool.execute(`INSERT INTO ingrediente(Nombre, Existencias, Existencias_Minimas) VALUES ('${req.body.Nombre}','${req.body.Existencias}','${req.body.Existencias_Minimas}')`);
-    res.send(responseBD);
+    res.send("Ingrediente agregado con exito");
 });
-app.put('/editar/:id_Ingrediente', async (req,res) => {
+app.put('/ingrediente/:id_Ingrediente', async (req,res) => {
     const ID = req.params.id_Ingrediente;
     const [responseBD] = await pool.execute(`UPDATE ingrediente SET Nombre='${req.body.Nombre}',Existencias='${req.body.Existencias}',Existencias_Minimas='${req.body.Existencias_Minimas}' WHERE id_Ingrediente=${ID}`);
-    res.send(responseBD);
+    res.send(`Se ha modificado el ingrediente: ${ID}`);
 });
-app.delete('/borrar/:id_Ingrediente', async (req,res) => {
+app.delete('/ingrediente/:id_Ingrediente', async (req,res) => {
     const ID = req.params.id_Ingrediente;
     const [responseBD] = await pool.execute(`DELETE FROM ingrediente WHERE id_Ingrediente=${ID}`);
-    res.json(responseBD);
+    res.send(`Se ha eliminado el ingrediente: ${ID}`);
 });
 
 app.listen(8082, () => {
