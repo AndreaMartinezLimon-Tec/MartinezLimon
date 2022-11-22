262b14554ec7fb4c64c187fc83fd72259b30b543
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-11-22 08:43:02 -0600

    JsDoc

---
+++ Parcial3/JsDoc/src/funciones.js
@@ -0,0 +1,10 @@
+/**
+ * Funcion que nos devuelve la potencia de un numero.
+ * @constructor
+ * @param {Int} base - El numero base el cual se va a exponenciar.
+ * @param {Int} exponente - El numero exponenciador.
+ */
+
+export function potencia(base,exponente) {
+    return base^exponente
+}
