23e941ddc824b3da2bec8fc05bac9397aafc8a0a
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-12-05 14:21:38 -0600

    Arreglando el swagger

--- API (Proyecto)/routes/RutaHerbolaria.js
+++ API (Proyecto)/routes/RutaHerbolaria.js
@@ -38,7 +38,7 @@ router.get('/', async (req,res) => {
 });
 /**
  * @swagger
- * /herbolaria/:ID:
+ * /herbolaria/{ID}:
  *   get:
  *     summary: Obtener información de una hierba por ID.
  *     tags:
@@ -52,12 +52,12 @@ router.get('/', async (req,res) => {
  *       500:
  *         description: Error del servidor.
  *   parameters:
- *      - name: id
+ *      - name: ID
  *        in: path
  *        required: true
  *        description: ID de la hierba.
  *        schema:
- *          type: int
+ *          type: integer
  */
 router.get('/:ID', async (req,res) => {
     try {
@@ -121,98 +121,98 @@ router.post('/hierba', async (req,res) => {
         res.status(500).json({ error: error.message });
     }
 });
-/**
- * @swagger
- * /herbolaria/ID:
- *   put:
- *     summary: Actualiza una hierba.
- *     tags:
- *        - Hierbas
- *     description: Actualiza la información de una hierba dado su ID
- *     produces:
- *       - text
- *     responses:
- *       200:
- *         description: Devuelve un mensaje confirmando que se actualizó la información.
- *       500:
- *         description: Error del servidor.
- *     parameters:
- *        - name: id
- *          in: path
- *          required: true
- *          description: ID de la hierba a actualizar.
- *          schema:
- *            type: string
- *        - name: Hierba
- *          in: body
- *          description: Información de la hierba por actualizar.
- *          required: true
- *          schema:
- *           type: object
- *           properties:
- *             Nombre_comun:
- *               type: string
- *               description: El nombre comùn de la planta.
- *             Nombre_cientifico:
- *               type: string
- *               description: El nombre cientifico de la hierba.
- *             Ubicación:
- *               type: string
- *               description: La mejor ubicación para plantar la hierba.
- *             Propagación:
- *               type: string
- *               description: El mètodo de propagación de la hierba.
- *             Mantenimiento:
- *               type: string
- *               description: El mantenimiento requerido por la hierba.
- *             Plagas:
- *               type: string
- *               description: Las plagas potenciales que pueden afectar a la hierba.
- *             Cosecha:
- *               type: string
- *               description: El mètodo para cosechar la hierba.
- */
-router.put('/:ID', async (req,res) => {
-    try {
-        const ID = req.params.ID;
-        const [responseBD] = await pool.execute(`UPDATE hierbas SET Nombre_comun='${req.body.Nombre_comun}',Nombre_cientifico='${req.body.Nombre_cientifico}',Ubicación='${req.body.Ubicación}',Propagación='${req.body.Propagación}',Mantenimiento='${req.body.Mantenimiento}',Plagas='${req.body.Plagas}',Cosecha='${req.body.Cosecha}' WHERE ID = ${ID}`);
-        res.send(`Se ha modificado la hierba: ${ID}`);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-/**
- * @swagger
- * /herbolaria/:ID:
- *   delete:
- *     summary: Eliminar una hierba por ID.
- *     tags:
- *        - Hierbas
- *     description: Obtener la informacion de un hierba especifica por medio de su ID.
- *     produces:
- *       - text
- *     responses:
- *       200:
- *         description: Retorna un mensaje confirmando la eliminación de la hierba.
- *       500:
- *         description: Error del servidor.
- *   parameters:
- *      - name: id
- *        in: path
- *        required: true
- *        description: ID de la hierba a eliminar.
- *        schema:
- *          type: int
- */
-router.delete('/:ID', async (req,res) => {
-    try {
-        const ID = req.params.ID;
-        const [responseBD] = await pool.execute(`DELETE FROM hierbas WHERE ID=${ID}`);
-        res.send(`Se ha eliminado la hierba: ${ID} `);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+///**
+// * @swagger
+// * /herbolaria/actualizar/{ID}:
+// *   put:
+// *     summary: Actualiza una hierba.
+// *     tags:
+// *        - Hierbas
+// *     description: Actualiza la información de una hierba dado su ID
+// *     produces:
+// *       - text
+// *     responses:
+// *       200:
+// *         description: Devuelve un mensaje confirmando que se actualizó la información.
+// *       500:
+// *         description: Error del servidor.
+// *     parameters:
+// *        - name: ID
+// *          in: path
+// *          required: true
+// *          description: ID de la hierba a actualizar.
+// *          schema:
+// *            type: integery
+// *        - name: Hierba
+// *          in: body
+// *          description: Información de la hierba por actualizar.
+// *          required: true
+// *          schema:
+// *           type: object
+// *           properties:
+// *             Nombre_comun:
+// *               type: string
+// *               description: El nombre comùn de la planta.
+// *             Nombre_cientifico:
+// *               type: string
+// *               description: El nombre cientifico de la hierba.
+// *             Ubicación:
+// *               type: string
+// *               description: La mejor ubicación para plantar la hierba.
+// *             Propagación:
+// *               type: string
+// *               description: El mètodo de propagación de la hierba.
+// *             Mantenimiento:
+// *               type: string
+// *               description: El mantenimiento requerido por la hierba.
+// *             Plagas:
+// *               type: string
+// *               description: Las plagas potenciales que pueden afectar a la hierba.
+// *             Cosecha:
+// *               type: string
+// *               description: El mètodo para cosechar la hierba.
+// */
+//router.put('/actualizar/:ID', async (req,res) => {
+//    try {
+//        const ID = req.params.ID;
+//        const [responseBD] = await pool.execute(`UPDATE hierbas SET Nombre_comun='${req.body.Nombre_comun}',Nombre_cientifico='${req.body.Nombre_cientifico}',Ubicación='${req.body.Ubicación}',Propagación='${req.body.Propagación}',Mantenimiento='${req.body.Mantenimiento}',Plagas='${req.body.Plagas}',Cosecha='${req.body.Cosecha}' WHERE ID = ${ID}`);
+//        res.send(`Se ha modificado la hierba: ${ID}`);
+//    } catch (error) {
+//        res.status(500).json({ error: error.message });
+//    }
+//});
+//
+///**
+// * @swagger
+// * /herbolaria/eliminar/{ID}:
+// *   delete:
+// *     summary: Eliminar una hierba por ID.
+// *     tags:
+// *        - Hierbas
+// *     description: Obtener la informacion de un hierba especifica por medio de su ID.
+// *     produces:
+// *       - text
+// *     responses:
+// *       200:
+// *         description: Retorna un mensaje confirmando la eliminación de la hierba.
+// *       500:
+// *         description: Error del servidor.
+// *   parameters:
+// *      - name: ID
+// *        in: path
+// *        required: true
+// *        description: ID de la hierba a eliminar.
+// *        schema:
+// *          type: int
+// */
+// router.delete('/eliminar/:ID', async (req,res) => {
+//    try {
+//        const ID = req.params.ID;
+//        const [responseBD] = await pool.execute(`DELETE FROM hierbas WHERE ID=${ID}`);
+//        res.send(`Se ha eliminado la hierba: ${ID} `);
+//    } catch (error) {
+//        res.status(500).json({ error: error.message });
+//    }
+//});
 
 module.exports.router=router;
